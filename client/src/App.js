f9478a2e5b9d757e4ce6de23893b48ccda1db1cb
POOJASHREE28 <poojapoojashree00@gmail.com>
2023-10-28 17:51:33 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,24 @@ import React from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Livetracking from './Components/Livetracking';
-
-// import Chart from './Components/Chart'
 import Signup from './Components/Signup';
 import Login from './Components/Login';
 import Home from './Components/Home';
+
+import Livetracking from './Components/Livetracking';
 import Scheduling from './Components/Scheduling';
+
+
 function App() {
   return (
    <BrowserRouter>
    <Routes>
-    {/* <Route path='/' element={<Livetracking/>}/> */}
-     <Route path='/*' element={<Signup/>}/> 
+     {/* <Route path='/*' element={<Signup/>}/> 
     <Route path='/login' element={<Login/>}/> 
-    <Route path='/home' element={<Home/>}/> 
-    <Route path='/scheduling' element={<Scheduling/>}/> 
+    <Route path='/home' element={<Home/>}/>  */}
+    {/* <Route path='/scheduling' element={<Scheduling/>}/>  */}
+
+    <Route path='/' element={<Livetracking/>}/>
     {/*<Route path='/' element={<Chart/>}/> */}
    
    </Routes>
---
+++ client/src/Components/Scheduling.js
@@ -0,0 +1,93 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+// import TimePicker from "react-time-picker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const locales = {
+    "en-IN": require("date-fns/locale/en-IN"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Bangalore",
+        allDay: true,
+        start: new Date(2023, 10, 0),
+        end: new Date(2023, 10, 0),
+    },
+    {
+        title: "Udupi",
+        start: new Date(2023, 10, 7),
+        end: new Date(2023, 10, 10),
+    },
+    {
+        title: "Dharmastala",
+        start: new Date(2023, 10, 20),
+        end: new Date(2021, 10, 23),
+    },
+];
+
+function Scheduling() {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        
+        for (let i=0; i<allEvents.length; i++){
+
+            const d1 = new Date (allEvents[i].start);
+            const d2 = new Date(newEvent.start);
+            const d3 = new Date(allEvents[i].end);
+            const d4 = new Date(newEvent.end);
+      /*
+          console.log(d1 <= d2);
+          console.log(d2 <= d3);
+          console.log(d1 <= d4);
+          console.log(d4 <= d3);
+            */
+
+             if (
+              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
+                (d4 <= d3) )
+              )
+            {   
+                alert("CLASH"); 
+                break;
+             }
+    
+        }
+        
+        
+        setAllEvents([...allEvents, newEvent]);
+    }
+
+    return (
+        <div className="App">
+            <h1>Bus Schedule</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        </div>
+    );
+}
+
+export default Scheduling;
